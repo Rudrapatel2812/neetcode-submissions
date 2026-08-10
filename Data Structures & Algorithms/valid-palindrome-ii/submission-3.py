cf3949de5121199ee57a7e118c1b4dc02a2018b5
Rudra Patel <80376731+Rudrapatel2812@users.noreply.github.com>
2026-08-09 18:28:31 -0700

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.py
@@ -0,0 +1,37 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        l=0
+        r=len(s)-1
+
+        def is_pali(l,r):
+            # while l<r:
+            #     if s[l]!=s[r]:
+            #         return False
+            #     l+=1
+            #     r-=1
+            # return True
+            while l<r:
+                if s[l]==s[r]:       
+                    l+=1
+                    r-=1
+                else:
+                    return False
+            return True
+
+
+        while l<r:
+            if s[l]==s[r]:
+                l+=1
+                r-=1
+            else:
+                return (is_pali(l+1,r) or is_pali(l,r-1))
+        return True
+        
+
+
+            
+            
+
+
+        
+        
